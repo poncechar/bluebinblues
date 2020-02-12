fe407549ea7de66663373ae4c43b8e2b3d08df65
Charmaine Ponce <charbar@Charmaines-MacBook-Pro.local>
2020-02-12 00:28:57 -0500

    added.... some js..

---
+++ script.js
@@ -0,0 +1,100 @@
+function blueBinQuiz(){};
+function showResults(){};
+
+const quizQuestions = [
+   {
+      question: 'Which of the following should *!~NEVER~!* go in the blue bin ? (HINT: this particular item is one of the TOP blue bin offenders)',
+      choices: {
+         a: 'assets/question1/R-can.jpg',
+         b: 'assets/question1/R-milk-bottles.jpg',
+         c: 'assets/question1/N-coffee-cup.jpg',
+         d: 'assets/question1/R-beer-bottles.jpg'
+      },
+      correctAnswer: 'c'
+   },
+   {
+      question: 'Approximately 30% of what is put in the blue bin doesn’t belong there. Are you able to choose the RECYCLABLE ITEM out of these options?',
+      choices: {
+         a: 'assets/question2/N-broken-glass.jpg',
+         b: 'assets/queston2/N-coffee-pods.jpg',
+         c: 'assets/question2/N-coffee-bags.jpg',
+         d: 'assets/question2/R-water-bottles.jpg'
+      },
+      correctAnswer: 'd'
+      },
+   {
+      question: 'Not all packaging is the same. Which item is NOT ACCEPTED in the blue bin?',
+      choices: {
+         a: 'assets/question3/N-icecream.jpg',
+         b: 'assets/question3/R-eggcarton.jpg',
+         c: 'assets/question3/R-mail-package.jpg',
+         d: 'assets/question3/R-milkcarton.jpg'
+      },
+      correctAnswer: 'a'
+      },
+   {
+      question: 'Take - away food containers can easily contaminate the recycling stream if not carefully disposed of.Contrary to popular belief, unfortunately not all are recyclable 🥺 Which container belongs in the blue bin(WHEN FULLY RINSED)?',
+      choices: {
+         a: 'assets/question4/R-styrofoam.jpg',
+         b: 'assets/question4/N-blackplastic.jpg',
+         c: 'assets/question4/N-papertogo.jpg',
+         d: 'assets/question4/N-pizzabox.jpg'
+      },
+      correctAnswer: 'a'
+   },
+   {
+      question: 'It can be tricky figuring out what kinds of food packaging are actually recyclable when grocery shopping. Which of these is recyclable and DOES *NOT* BELONG in the landfill?',
+      choices: {
+         a: 'assets/question5/N-frozen-boxes.jpg',
+         b: 'assets/question5/N-chipbag.jpg',
+         c: 'assets/question5/R-saladpackaging.jpg',
+         d: 'assets/question5/N-standup.jpg'
+      },
+      correctAnswer: 'c'
+   },
+   {
+      question: 'Not all paper is created equal! Which NON-RECYCLABLE PAPER ITEM continually sneaks into our blue bins?',
+      choices: {
+         a: 'assets/question6/N-receipt.jpg',
+         b: 'assets/question6/R-shoppingbag.jpg',
+         c: 'assets/question6/R-magazines.jpg',
+         d: 'assets/question6/R-books.jpg'
+      },
+      correctAnswer: 'a'
+   },
+   {
+      question: 'Along with paper materials, not all plastics are recyclable. Canada recycles just 9% of its plastics with the rest dumped in landfill and incinerators or tossed away as litter. Can you guess which commonly used plastic is THE MOST COMPLICATED to recycle across the board?',
+      choices: {
+         a: 'assets/question7/N-toothbrush.jpg',
+         b: 'assets/question7/R-plasticbags.jpg',
+         c: 'assets/question7/R-icedcoffee.jpg',
+         d: 'assets/question7/R-shampoo.jpg'
+      },
+      correctAnswer: 'a'
+   },
+   {
+      question: 'And lastly, which plastic cannot be sorted mechanically at the recycling facility because the optical sorting technology simply CANNOT RECOGNIZE IT?',
+      choices: {
+         a: 'assets/question8/R-bottlecaps.jpg',
+         b: 'assets/question8/R-lotionbottle.jpg',
+         c: 'assets/question8/R-juice.jpg',
+         d: 'assets/question8/N-blackplastic.jpg'
+      },
+      correctAnswer: 'd'
+   }
+   ];
+
+// console.log(quizQuestions[1]);
+
+const currentQuestion = 0;
+const correctAnswers = 0;
+// const quizScore = 0;
+
+function setupQuiz() {
+   const htmlToAppend = []
+
+   quizQuestions.forEach(
+
+   )
+};
+
